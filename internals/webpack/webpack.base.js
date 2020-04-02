878eb22a0ddf52b49ca5732a5e181a42b6175307
HajimeNoJosh <joshuah.m.martin@gmail.com>
2020-04-02 16:44:52 -0400

    Add Sass integration
    These three loaders work together to make sass work.
    
    Sass-loader allows you to control the versions of all your dependencies,
    and to choose which Sass implementation to use.
    
    Css-loader interprets @import and url() like import/require()
    and will resolve them.
    
    Style-loader injects CSS into the DOM.
    
    Also implemented basic 7-1 folder structure for Sass.
    
    Resolves #7

--- internals/webpack/webpack.base.js
+++ internals/webpack/webpack.base.js
@@ -22,6 +22,17 @@ module.exports = (options) => ({
           },
         ],
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      }
     ],
   },
   devServer: options.devServer,
