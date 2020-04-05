749ad1faa047b9cbafa9d151533be871bbc4d2f7
Pauline Masigla <pmasigla@oreilly.com>
2020-04-05 17:19:50 -0400

    Add Sass support for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,14 @@
+const path = require('path');
+
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
+  stories: ['../src/components/**/*.stories.jsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
   webpackFinal: async config => {
-    // do mutation to the config
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../')
+    });
 
     return config;
   },
---
+++ .storybook/preview.js
@@ -0,0 +1,2 @@
+import React from 'react';
+import '../src/sass/main.scss';
