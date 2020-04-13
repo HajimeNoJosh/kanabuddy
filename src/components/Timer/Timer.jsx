ed16f0d74925ea0bd35ce5ce8e8baca43effe918
HajimeNoJosh <joshuah.m.martin@gmail.com>
2020-04-13 17:44:44 -0400

    Fix spaces and Deconstruct props
    Fixed spacing in scss file and destructured props

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './Timer.scss';
 
 const Timer = (props) => {
+  const { hours, minutes } = props;
   return (
     <div className="timer">
       <div className="timer__label">Timer</div>
       <div className="timer__digits">
-        {props.hours}:{props.minutes}
+        {hours}:{minutes}
       </div>
     </div>
   );
