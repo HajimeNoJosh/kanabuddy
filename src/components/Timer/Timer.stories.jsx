e570a199365795cfebd91a27e2873145b4f91a64
HajimeNoJosh <joshuah.m.martin@gmail.com>
2020-04-16 00:00:16 -0400

    Implement knobs on Timer

--- src/components/Timer/Timer.stories.jsx
+++ src/components/Timer/Timer.stories.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { number } from '@storybook/addon-knobs';
 import Timer from './Timer.jsx';
 
 export default {
@@ -7,4 +7,16 @@ export default {
   component: Timer,
 };
 
-export const time = () => <Timer hours={1} minutes={45} />;
+export const Default = () => {
+  const labelMinute = 'Minutes';
+  const defaultMinute = 0;
+
+  const minute = number(labelMinute, defaultMinute);
+
+  const labelSecond = 'Seconds';
+  const defaultSecond = 0;
+
+  const second = number(labelSecond, defaultSecond);
+
+  return <Timer minutes={minute} seconds={second} />;
+};
