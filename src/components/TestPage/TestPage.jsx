bcbfd69c138cbac015305a9e6a0ca38ec7fa5d10
Josh martin <48595872+HajimeNoJosh@users.noreply.github.com>
2020-05-06 14:52:29 -0400

    Implement buttons on testpage (#42)
    * Implement buttons on testpage
    
    Made a temporary input with a function startTimer(). This is only
    temporary and will be cleaned up later.
    
    Currently the button functions the way it does through a new state
    isPauseButtonShowing. This state dictates whether to show the pause
    button or to show the start/restart buttons. It does so in the toggle
    function. It just sets pauseButtonShowing to the opposite of isActive.
    
    * implement ternary to clean up code

--- src/components/TestPage/TestPage.jsx
+++ src/components/TestPage/TestPage.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Scaffold } from '../Scaffold/Scaffold';
+import { Button } from '../Button/Button';
 
 import './TestPage.scss';
 
 export const TestPage = () => {
   const [time, setTime] = useState({ m: 1, s: 0 });
   const [isActive, setIsActive] = useState(false);
+  const [isPauseShowing, setIsPauseShowing] = useState(true);
   const [timerId, setTimerId] = useState(null);
 
   useEffect(() => {
@@ -20,12 +22,14 @@ export const TestPage = () => {
 
   function toggle() {
     setIsActive(!isActive);
+    setIsPauseShowing(!isActive);
   }
 
   const restart = () => {
     setTime({ m: 1, s: 0 });
     clearInterval(timerId);
     setIsActive(false);
+    setIsPauseShowing(true);
   };
 
   const tick = () => {
@@ -40,28 +44,46 @@ export const TestPage = () => {
     }
   };
 
+  const startTimer = () => {
+    if (time.m === 1) {
+      tick();
+      toggle();
+    }
+  };
+
   return (
     <Scaffold minutes={time.m} seconds={time.s}>
+      <input onKeyDown={startTimer}></input>
       <div className="test">
-        <button
-          aria-label="play-button"
-          disabled={isActive}
-          onClick={toggle}
-          type="button"
-        >
-          Play
-        </button>
-        <button
-          aria-label="pause-button"
-          disabled={!isActive}
-          onClick={toggle}
-          type="button"
-        >
-          Pause
-        </button>
-        <button aria-label="restart-button" onClick={restart} type="button">
-          Restart
-        </button>
+        {isPauseShowing ? (
+          <Button
+            onClick={toggle}
+            aria-label="pause-button"
+            disabled={!isActive}
+            variant="primary"
+            iconName="pause"
+            text="Pause"
+          />
+        ) : (
+          <div>
+            <Button
+              onClick={toggle}
+              aria-label="play-button"
+              disabled={isActive}
+              variant="primary"
+              iconName="start"
+              text="Start"
+            />
+            <Button
+              onClick={restart}
+              aria-label="restart-button"
+              disabled={isActive}
+              variant="secondary"
+              iconName="restart"
+              text="Restart"
+            />
+          </div>
+        )}
       </div>
     </Scaffold>
   );
