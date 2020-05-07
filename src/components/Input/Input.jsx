1ed7080afd010b32e6bc379fdc86fa9abf28bc22
HajimeNoJosh <joshuah.m.martin@gmail.com>
2020-05-07 18:11:47 -0400

    add diabled functionality to input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 
 import './Input.scss';
 
-export const Input = ({ ariaLabel, maxLength, onKeyDown, ref }) => (
+export const Input = ({ ariaLabel, maxLength, disabled, onKeyDown, ref }) => (
   <input
     type="text"
     aria-label={ariaLabel}
     className="input"
     maxLength={maxLength}
+    disabled={disabled}
     onKeyDown={onKeyDown}
     ref={ref}
   />
@@ -18,5 +19,6 @@ Input.propTypes = {
   onKeyDown: PropTypes.func,
   ariaLabel: PropTypes.string,
   maxLength: PropTypes.string,
+  disabled: PropTypes.bool,
   ref: PropTypes.func,
 };
